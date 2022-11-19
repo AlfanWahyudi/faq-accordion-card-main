d11d0129830d4ac1e96e51a19f93e420361ebbbe
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-11-19 14:57:11 +0700

    update handle toggle list FAQ

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const btnQuestions = document.querySelectorAll('#btnQuestion');
 
 btnQuestions.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    const answerELem = document.querySelector(`#${e.target.value}`);
+    const answerELem = btn.nextElementSibling;
     answerELem.toggleAttribute('hidden');
+    btn.classList.toggle('btn-clicked');
+
+    const imgArrow = btn.lastElementChild;
+    imgArrow.classList.toggle('arrow-up')
   });
 });
