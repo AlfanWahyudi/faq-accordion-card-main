d474926d939cc241c0ff116f898ba3cd49d09ab8
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-11-22 21:52:33 +0700

    update expand answer when click question

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 const btnQuestions = document.querySelectorAll('#btnQuestion');
 
-btnQuestions.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    const answerELem = btn.nextElementSibling;
-    answerELem.toggleAttribute('hidden');
-    btn.classList.toggle('btn-clicked');
+const toggleFaqHeight = (currentHeight, actualHeight, answerHeight) => {
+  const minHeight = 15;
+  return currentHeight !== actualHeight ? `${answerHeight}px` : `${minHeight}px`;
+}
 
-    const imgArrow = btn.lastElementChild;
-    imgArrow.classList.toggle('arrow-up')
-  });
+const setHeightFaqItem = (faqItem, answerElem) => {
+  const paddingFaq = 17;
+  const btnQestionHeight = 18;
+
+  const answerHeight = answerElem.offsetHeight + paddingFaq;
+  const actualFaqHeight = answerHeight + paddingFaq + btnQestionHeight;
+  
+  faqItem.style.height = toggleFaqHeight(faqItem.offsetHeight, actualFaqHeight, answerHeight);
+}
+
+const rotatingArrowIcon = (imgArrow) => {
+  imgArrow.classList.toggle('arrow-up');
+}
+
+const toggleExpandAnswer = (event) => {
+  const btn = event.srcElement;
+  const imgArrow = btn.lastElementChild;
+  const faqItem = btn.parentElement;
+  const answerElem = btn.nextElementSibling;
+  
+  btn.classList.toggle('btn-clicked');
+  setHeightFaqItem(faqItem, answerElem);
+  rotatingArrowIcon(imgArrow);
+}
+
+btnQuestions.forEach(btn => {
+  btn.addEventListener('click', toggleExpandAnswer);
 });
